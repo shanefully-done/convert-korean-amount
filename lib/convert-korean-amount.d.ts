c831b8bff22af51ec206a9ad1c8b148de44c8209
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2024-10-21 18:25:35 +0900

    feat: added comma option

--- lib/convert-korean-amount.d.ts
+++ lib/convert-korean-amount.d.ts
@@ -1,6 +1,7 @@
 export declare type Options = {
     wordSpacing?: boolean;
     withWonSuffix?: boolean;
+    useComma?: boolean;
 };
-declare const convertKoreanAmount: (value: number, { wordSpacing, withWonSuffix }?: Options) => string;
+declare const convertKoreanAmount: (value: number, { wordSpacing, withWonSuffix, useComma }?: Options) => string;
 export default convertKoreanAmount;
